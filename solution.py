7003a7c72bf8b6c76645d227c3df8a27ccdd2d2c
Anna <lisachenko.am@phystech.edu>
2024-03-28 11:55:27 +0300

    intermediate solution

--- solution.py
+++ solution.py
@@ -2,6 +2,9 @@ import numpy as np
 
 def get_predict(df):
     lr_coef = np.load('coef.npy')
-    SANHOK_columns = [column for column in df.columns if column.startswith('SANHOK')]
+    SANHOK_columns = []
+    for i in range(3):
+        SANHOK_columns.append(f'SANHOK_bid{i}_price')
+        SANHOK_columns.append(f'SANHOK_ask{i}_price')
     y_pred = np.matmul(df[SANHOK_columns], lr_coef)
     return y_pred
